18e792475d8fe71f4b16fc74453610c97c81b1b1
7aisal <7aisal@7aisal-PC>
2018-12-30 04:45:08 -0600

    These are the entities created hoping to reflect them in my database.
    The pom.xml was modified to include the dependancies preventing the
    build from succeeding and the tomcat server from running.
    
    The changes to the pom.xml do not match the tutorial video as his did
    not need these modifications for some reason.

---
+++ FreshVotes/src/main/java/com/freshvotes/domain/Feature.java
@@ -0,0 +1,45 @@
+package com.freshvotes.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Feature {
+	private Long id;
+	private String title;
+	private String description;
+	private String name;
+	
+	
+	@Id @GeneratedValue (strategy=GenerationType.IDENTITY)
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+
+}
---
+++ FreshVotes/src/main/java/com/freshvotes/domain/Vote.java
@@ -0,0 +1,22 @@
+package com.freshvotes.domain;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+
+@Entity
+public class Vote {
+	
+	@EmbeddedId
+	private VoteId pk;
+	private Boolean upvote;
+
+	public Boolean getUpvote() {
+		return upvote;
+	}
+
+	public void setUpvote(Boolean upvote) {
+		this.upvote = upvote;
+	}
+	
+
+}
